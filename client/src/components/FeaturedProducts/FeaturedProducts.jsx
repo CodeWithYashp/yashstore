718eba56a6ea8e836c6dcc50255ddd4e919e8c11
CodeWithYashp <ue205131.yash.ece@gmail.com>
2023-10-02 03:20:26 +0530

    first commit: home page design done

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./FeaturedProducts.scss";
+import Card from "../Card/Card";
+
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long Sleeve Graphic T-Shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Coat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Skirt",
+      isNew: false,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Hat",
+      isNew: false,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum
+          debitis sit sint atque sapiente natus error nihil iure, impedit illum
+          animi ea corrupti nisi perferendis nemo beatae id tenetur voluptatum!
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map((item) => (
+          <Card item={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
